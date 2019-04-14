e87d4d5d14690d2732a2e46cbb368d067d28b944
Ezzeldin Tahoun <15705851+ezzeldinadel@users.noreply.github.com>
2019-04-13 22:10:10 -0400

    Update ezQOS.sh

--- QOS/ezQOS.sh
+++ QOS/ezQOS.sh
@@ -3,7 +3,7 @@
 
 while true
 do
-        com="$(ping "$1" -c1 | grep 64 | awk 'FNR == 1 {print $7}' | sed -n 's/^time=//p')"
+        com="$(ping "$1" -c5 | grep 64 | awk 'FNR == 5 {print $7}' | sed -n 's/^time=//p')"
         echo "${com}" >> QOSlog.txt
         sleep 10s
 done    
