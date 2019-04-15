73375644b5489e05f443aed1aec2d614ac3c3bf1
Ezzeldin Tahoun <15705851+ezzeldinadel@users.noreply.github.com>
2019-04-14 23:59:49 -0400

    Update brains_rl.py

--- brains_rl.py
+++ brains_rl.py
@@ -1,17 +1,23 @@
 __author__ = 'philippe'
-import World
+#import World
 import threading
 import time
 
 score =1 
 state =(3,3)
 
+init_VN_h1="VNB"
+init_VN_h2="VNB"
+curr_VN_h1="VNB"
+curr_VN_h2="VNB"
+
 #h1 is attacker 
 #h2 is benign 
+#both start on vnb 
 ''' --- STATES ---
 
-## u for up   ( too many occurences) 
-## d for down ( too lil  occurences) 
+## u for up   ( too many occurrences ) 
+## d for down ( too lil  occurrences ) 
 states=[  
 IDS--h1u--h2u = 0 
 IDS--h1u--h2d = 1 
@@ -23,11 +29,12 @@ IPS--h1u--h2d = 1
 IPS--h1d--h2u = 2 
 IPS--h1d--h2d = 3 
 ]
+
 state = (IDS,IPS) 
 Example:- 
 (2,3) = IDS--h1d--h2u AND IPS--h1d--h2d meaning 
-IDS alerts have too little occurences of h1 and too many occurences of h2 
-IPS alerts have too little occurences of h1 and too little occurences of h2 
+IDS alerts have too little occurrences  of h1 and too many occurrences  of h2 
+IPS alerts have too little occurrences  of h1 and too little occurrences  of h2 
 '''
 
 ''' --- ACTIONS ---
@@ -55,7 +62,7 @@ specials = [(0, 3, "bad", -1), # IDS--h1u--h2u, IPS--h1d--h2d ## BOTH ATTACKING 
 
 Q = {}
 discount = 0.3
-actions = ["toggle_h1","toggle_h2"] #World.actions
+actions = ["toggle_h1","toggle_h2", "nothing"] #World.actions
 states = []
 for i in range(4):#World.x):
     for j in range(4):#(World.y):
@@ -74,18 +81,57 @@ for (i, j, c, w) in specials: #World.specials:
         #World.set_cell_score((i, j), action, w)
 
 def toggle (h): 
+	global curr_VN_h1, curr_VN_h2
+	print "-- toggle = " + h
 	#iman fuck u 
 	#please implement the toggle function api call here sir 
 	#h will be "h1" or "h2"
+	if h == "h1": 
+		if curr_VN_h1 == "VNM": 
+			curr_VN_h1 = "VNB"
+		elif curr_VN_h1 == "VNB": 
+			curr_VN_h1 = "VNM"
+	elif h == "h2": 
+		if curr_VN_h2 == "VNM": 
+			curr_VN_h2 = "VNB"
+		elif curr_VN_h2 == "VNB": 
+			curr_VN_h2 = "VNM"
+
+def GET_IDS_occurrences (): 
+	'''
+	Reads IDS alerts/logs from a SQL DB running connected in real time to the logs from the IDS deployed on the VN Benign 
+	Return 0 if up for h1 and h2 
+	Return 1 if up for h1 and down for h2 
+	Return 2 if down for h1 and up for h2 
+	Return 3 if down  for h1 and h2 
+	'''
+	return 1
+
+def GET_IPS_occurrences (): 
+	'''
+	Reads IPS alerts/logs from a SQL DB running connected in real time to the logs from the IPS deployed on the VN Benign 
+	Return 0 if up for h1 and h2 
+	Return 1 if up for h1 and down for h2 
+	Return 2 if down for h1 and up for h2 
+	Return 3 if down  for h1 and h2 
+
+	'''
+	return 1
+
 
 def update_state(): 
 	#api call to get the status of IDS and IPS 
 	# return a tuple of two numbers 
 	# example = (0,0) == IDS--h1u--h2u, IPS--h1u--h2u ## BOTH ATTACKING IDS & IPS  
+	d = GET_IDS_occurrences ()
+	p = GET_IDS_occurrences ()
+	new_state = (d,p)
+	
 	reward() #update the score for this round 
 	return new_state 
 
 def reward(): 
+	global score 
 	#update score 
 	# call QOS for h1 and h2 
 	# give me a score that shows better score for better QOS on h2 and worse on h1 please 
@@ -97,15 +143,15 @@ def reward():
 		if state == (i,j):
 			score += w
 			if score > 0:
-                print "Success! score: ", score
+				print "Success! score: ", score
 				restart_net()
-            else:
-                print "Fail! score: ", score
+			else:
+				print "Fail! score: ", score
 				restart_net()
 
 def restart_net(): 
+	print "========= RESTARTEDDDDD =========" 
 	#restarts state 
-	curr_VN_h1=api_VN("h1") 
 	if init_VN_h1 != curr_VN_h1: toggle("h1") 
 	if init_VN_h2 != curr_VN_h2: toggle("h2") 
 	
@@ -123,6 +169,7 @@ def do_action(action):
     #elif action == actions[3]:
     #    World.try_move(1, 0)
     else:
+        print "do nothing" 
         return
     s2 = update_state() 
     r += score
@@ -142,7 +189,7 @@ def max_Q(s):
 def inc_Q(s, a, alpha, inc):
     Q[s][a] *= 1 - alpha
     Q[s][a] += alpha * inc
-    World.set_cell_score(s, a, Q[s][a])
+    #World.set_cell_score(s, a, Q[s][a]) -- have no clue tbh 
 
 
 def run():
@@ -152,7 +199,7 @@ def run():
     t = 1
     while True:
         # Pick the right action
-        s = World.player
+        s = state #World.player
         max_act, max_val = max_Q(s)
         (s, a, r, s2) = do_action(max_act)
 
@@ -172,17 +219,17 @@ def run():
 
 
         #debug life 
-        print (Q[(0,0)])
+        #print (Q[(0,0)])
         #print (Q[(0,1)])
 
 
         # MODIFY THIS SLEEP IF THE GAME IS GOING TOO FAST.
         time.sleep(0.1)
         
-        
 
 
-t = threading.Thread(target=run)
-t.daemon = True
-t.start()
-World.start_game()
+run()
+#t.daemon = True
+#t.start()
+
+#World.start_game()
